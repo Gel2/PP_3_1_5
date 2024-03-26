6318dfb432089cb513ad1667837ea3f764a09164
Gel2 <goncharovceo@mail.ru>
2024-03-26 12:20:39 +1000

    fix contr

--- PP_3_1_2_Boot_Security_bootstrap4-master/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ PP_3_1_2_Boot_Security_bootstrap4-master/src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -12,47 +12,58 @@ import ru.kata.spring.boot_security.demo.service.UserService;
 import java.security.Principal;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
     private final UserService userService;
     private final RoleService roleService;
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
 
-    @Autowired()
+    @Autowired
     public AdminController(UserService userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping
     public String getAdminPanel(Model model, @AuthenticationPrincipal User user) {
+        logger.info("Accessed admin panel by user: {}", user.getUsername());
         model.addAttribute("userList", userService.getAllUsers());
         model.addAttribute("roleList", roleService.getAllRoles());
         model.addAttribute("newUser", new User());
-        model.addAttribute("userRole", userService.findByUsername(user.getUsername()));
         return "adminPanel";
     }
 
-    @GetMapping("/admin/admin-info")
+    @GetMapping("/admin-info")
     public String getAdminProfile(Model model, Principal principal) {
+        logger.info("Accessed admin profile by user: {}", principal.getName());
         model.addAttribute("user", userService.findByUsername(principal.getName()));
         return "adminInfoPage";
     }
 
-    @PostMapping("/admin/create")
+    @PostMapping("/create")
     public String createUser(@ModelAttribute("user") User user) {
         userService.create(user);
+        logger.info("User created: {}", user.getUsername());
         return "redirect:/admin";
     }
 
-    @PutMapping("/admin/update/{id}")
+    @PutMapping("/update/{id}")
     public String updateUser(@ModelAttribute("user") User user) {
         userService.update(user);
+        logger.info("User updated: {}", user.getUsername());
         return "redirect:/admin";
     }
 
-    @DeleteMapping("/admin/delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id) {
+        User user = userService.getById(id);
         userService.delete(id);
+        logger.info("User deleted: {}", user.getUsername());
         return "redirect:/admin";
     }
 }
+
